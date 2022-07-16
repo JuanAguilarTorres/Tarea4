aef5580599b154a93e1aa21acf20507d08c69ed4
JuanAguilarTorres <Juan.aguilartorres@ucr.ac.cr>
2022-07-16 07:30:34 -0600

    Botón de importar

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,5 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "./../../lib/include/tienda.h"
+#include "./../../lib/include/producto.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <iostream>
+#include <fstream>
+
+//Inicio
+Tienda *laTienda = new Tienda();
+//---
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +21,34 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnImportar_clicked()
+{
+    QString archivoDireccion = QFileDialog::getOpenFileName(this, "Abrir archivo", QDir::homePath());
+    if (archivoDireccion == "")
+    {
+        QMessageBox::information(this, "Archivo", "Cancelado");
+    }else
+    {
+        QMessageBox::information(this, "Archivo", archivoDireccion);
+        QFile archivo(archivoDireccion);
+        if (!archivo.open(QFile::ReadOnly|QFile::Text))
+        {
+            QMessageBox::warning(this,"Error","El archivo no se abrió");
+        }
+        std::string stream = QTextStream(&archivo).readAll().toStdString();
+
+        std::filebuf fb;
+        if (fb.open (archivoDireccion.toStdString(),std::ios::in))
+          {
+            std::istream entrada(&fb);
+            while (entrada)
+              std::cout << char(entrada.get());
+            fb.close();
+            laTienda->CargarDesdeStreamBinario(&entrada);
+          }
+
+    }
 }
 
