595bf06b2727b5df4dc09ecd6f90afb05cd2bd4c
JuanAguilarTorres <Juan.aguilartorres@ucr.ac.cr>
2022-07-16 21:36:59 -0600

    Metodo de botón agregar

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -25,6 +25,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::actualizarLista()
 {
+    ui->listaProductos->clear();
     for (Producto *producto : laTienda->consultarProductos())
     {
        ui->listaProductos->addItem(QString::fromStdString(producto->obtenerString()));
@@ -33,30 +34,40 @@ void MainWindow::actualizarLista()
 
 void MainWindow::on_btnImportar_clicked()
 {
-    QString archivoDireccion = QFileDialog::getOpenFileName(this, "Abrir archivo", QDir::homePath());
-    if (archivoDireccion == "")
-    {
-        QMessageBox::information(this, "Archivo", "Cancelado");
-    }else
-    {
-        QMessageBox::information(this, "Archivo", archivoDireccion);
-        QFile archivo(archivoDireccion);
-        if (!archivo.open(QFile::ReadOnly|QFile::Text))
-        {
-            QMessageBox::warning(this,"Error","El archivo no se abrió");
-        }
-        std::string stream = QTextStream(&archivo).readAll().toStdString();
+//    QString archivoDireccion = QFileDialog::getOpenFileName(this, "Abrir archivo", QDir::homePath());
+//    if (archivoDireccion == "")
+//    {
+//        QMessageBox::information(this, "Archivo", "Cancelado");
+//    }else
+//    {
+//        QMessageBox::information(this, "Archivo", archivoDireccion);
+//        QFile archivo(archivoDireccion);
+//        if (!archivo.open(QFile::ReadOnly|QFile::Text))
+//        {
+//            QMessageBox::warning(this,"Error","El archivo no se abrió");
+//        }
+//        std::string stream = QTextStream(&archivo).readAll().toStdString();
 
-        std::filebuf fb;
-        if (fb.open (archivoDireccion.toStdString(),std::ios::in))
-          {
-            std::istream entrada(&fb);
-            while (entrada)
-              std::cout << char(entrada.get());
-            fb.close();
-            laTienda->CargarDesdeStreamBinario(&entrada);
-          }
+//        std::filebuf fb;
+//        if (fb.open (archivoDireccion.toStdString(),std::ios::in))
+//          {
+//            std::istream entrada(&fb);
+//            while (entrada)
+//              std::cout << char(entrada.get());
+//            fb.close();
+//            laTienda->CargarDesdeStreamBinario(&entrada);
+//          }
 
-    }
+//    }
+}
+
+
+void MainWindow::on_btnAgregar_clicked()
+{
+    laTienda->agregarProducto((this->ui->lineIdProducto->text()).toInt(), (this->ui->lineNombreProducto->text()).toStdString(), (this->ui->lineExistencia->text()).toInt());
+    this->ui->lineIdProducto->clear();
+    this->ui->lineNombreProducto->clear();
+    this->ui->lineExistencia->clear();
+    this->actualizarLista();
 }
 
